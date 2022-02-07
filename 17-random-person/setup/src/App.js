9772328b994c431e8036d0be7f99df9a6eb573a5
Tirth <tirthcoder@gmail.com>
2022-02-07 12:03:02 +0530

    random person and pagination app completed

--- 17-random-person/setup/src/App.js
+++ 17-random-person/setup/src/App.js
@@ -7,10 +7,107 @@ import {
   FaPhone,
   FaLock,
 } from 'react-icons/fa'
+
 const url = 'https://randomuser.me/api/'
 const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+
 function App() {
-  return <h2>random user starter</h2>
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState("name");
+  const [value, setValue] = useState("random person");
+
+  const handleValue = (e) => {
+    // Targeting button
+    if(e.target.classList.contains("icon")){
+      const newValue = e.target.dataset.label;
+      setTitle(newValue);
+      setValue(person[newValue]);
+    }
+  }
+
+  const getPerson = async() => {
+    const res = await fetch(url);
+    const data = await res.json();
+    const person = data.results[0];
+    const {first, last} = person.name;
+    const {street: {name, number}} = person.location;
+    const {email, phone} = person;
+    const {password} = person.login;
+    const {age} = person.dob;
+    const {large: image} = person.picture;
+
+    const newPerson = {
+      image,
+      street: `${number} ${name}`,
+      email, 
+      phone,
+      password,
+      age,
+      name: `${first} ${last}`
+    }
+
+    setPerson(newPerson);
+    setLoading(false);
+    setTitle("name");
+    setValue(newPerson.name);
+  }
+
+  useEffect(() => {
+    getPerson();
+  }, [])
+
+  return (
+    <main>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img src={(person && person.image) || defaultImage} alt="random user" className='user-img' />
+          <p className="user-title">My {title} is</p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+            <button className="icon" data-label='name' onMouseOver={handleValue}>
+              <FaUser />
+            </button>
+            <button
+              className='icon'
+              data-label='email'
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+            <button className='icon' data-label='age' onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              className='icon'
+              data-label='street'
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+            <button
+              className='icon'
+              data-label='phone'
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+            <button
+              className='icon'
+              data-label='password'
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </button>
+          </div>
+          <button className="btn" type='button' onClick={getPerson}>
+            {loading ? 'loading...' : 'random user'}
+          </button>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App
--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,14 @@
-const paginate = () => {}
+const paginate = (followers) => {
+    const itemsPerPage = 10;
+    const noOfPages = Math.ceil(followers.length / itemsPerPage);
+
+    // Make Array of Array [ [10 follower] [10 follower] ... ] instead of single arr [100 follower]
+    const newFollowers = Array.from({length: noOfPages}, (_, index) => {
+        const start = index * itemsPerPage;
+        return followers.slice(start, start + itemsPerPage);
+    })
+
+    return newFollowers;
+}
 
 export default paginate
